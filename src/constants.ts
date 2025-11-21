d11c24960f9aa389dadbc0a6a0d385bbe7bdf8a6
Steven Scaffidi <sscaff1@gmail.com>
2025-11-20 19:58:17 -0600

    chore: update youtube link

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const YOUTUBE_URL = 'https://www.youtube.com/@steven_scaffidi';
+export const YOUTUBE_URL = 'https://www.youtube.com/@Everesting4Rescues';
 export const ZWIFT_URL = 'https://www.zwift.com/athlete/182e9d46-fa33-4bdd-9ca1-348869fdb747';
 export const STRAVA_URL = 'https://www.strava.com/athletes/114359616';
 export const DONATION_URL = 'https://gofund.me/cec5e37d';
